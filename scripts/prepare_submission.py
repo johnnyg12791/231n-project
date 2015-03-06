80f116f55d6cda9864cafbb2d4445ed9ce933841
Alex Zamoshchin <alexzamoshchin@gmail.com>
2015-03-06 18:56:35 -0500

    prepare submission kinda works but slow

--- scripts/prepare_submission.py
+++ scripts/prepare_submission.py
@@ -1,9 +1,10 @@
-# TODO
-
 from scipy.misc import imread, imresize
 import numpy as np
 import os
 import sys
+import math
+
+BATCH_SIZE = 200
 
 # hack sys.path so we can import caffe
 caffe_python_path = '~/caffe/python'
@@ -11,7 +12,8 @@ sys.path.insert(0, caffe_python_path)
 
 # Set the default GPU that caffe will use.
 import caffe
-caffe.set_device(0)
+#caffe.set_device(0)
+caffe.set_mode_gpu()
 
 model_file = '../plankton_caffenet/plankton_deploy.prototxt'
 weights_file = '../plankton_caffenet/snapshot_iter_4501.caffemodel'
@@ -21,51 +23,52 @@ directory = '../data/test'
 outputFile = open('../data/submission.csv', 'w')
 
 images = [item for item in os.listdir(directory)]
-images = images[1:100]
+num_images = len(images)
+num_batches = int(math.ceil(num_images / BATCH_SIZE))
+
 input_data = np.zeros((len(images), 1, 227, 227))
-for i,image in enumerate(images):
-  if image == '.DS_Store': continue
-  print i
-  img = imread('../data/test/' + image)
+for batch_num in range(num_batches):
+  print "Batch:", batch_num, "/", num_batches
+  for i in range(BATCH_SIZE):
+    img_index = i + BATCH_SIZE * batch_num
+    if (img_index >= num_images): break
+    image = images[img_index]
+    
+    #if image == '.DS_Store': continue
+    img = imread('../data/test/' + image)
   
-  # Before feeding the image to the network, we need to preprocess it:
-  # 1) Resize image to (256, 256)
-  # 2) Swap channels from RGB to BGR (for CaffeNet)
-  # 3) Reshape from (H, W, K) to (K, H, W)
-  # 4) Subtract ImageNet mean
-  # 5) Crop or resize to (227, 227)
+    # Resize image to same size as mean
+    #H_mean, W_mean = mean.shape[1:]
+    # TODO
+    H_mean,W_mean = 256, 256
+    img = imresize(img, (H_mean, W_mean))
+    
+    # Reshape from (H, W, K) to (K, H, W)
+    #img = img.transpose(2, 0, 1)
+    
+    # Subtract mean
+    #img = img - mean
+    
+    # Crop to input size of network
+    H_in, W_in = net.blobs['data'].data.shape[2:]
+    H0 = (H_mean - H_in) / 2
+    H1 = H0 + H_in
+    W0 = (W_mean - W_in) / 2
+    W1 = W0 + W_in
+    img = img[H0:H1, W0:W1]
   
-  # Resize image to same size as mean
-  #H_mean, W_mean = mean.shape[1:]
-  # TODO
-  H_mean = 256
-  W_mean = 256
-  img = imresize(img, (H_mean, W_mean))
-  
-  # Reshape from (H, W, K) to (K, H, W)
-  #img = img.transpose(2, 0, 1)
-  
-  # Subtract mean
-  #img = img - mean
-  
-  # Crop to input size of network
-  H_in, W_in = net.blobs['data'].data.shape[2:]
-  H0 = (H_mean - H_in) / 2
-  H1 = H0 + H_in
-  W0 = (W_mean - W_in) / 2
-  W1 = W0 + W_in
-  img = img[H0:H1, W0:W1]
-  
-  # Copy input data to input blob of the network
-  input_data[i] = img
-  
-print net.blobs['data'].data.shape
-net.blobs['data'].data[0] = input_data
-  
+    # Copy input data to input blob of the network
+    net.blobs['data'].data[i] = img
+
   # Call net forward
-net.forward()
+  net.forward()
   
   # Pull out the probabilities from the network and print them
-probs = net.blobs['prob'].data[0]
-print probs[0:1]
-#outputFile.write(image +',' + ','.join(['%.16f' % num for num in probs]) + '\n')
+  probs = net.blobs['prob'].data
+  for i in range(BATCH_SIZE):
+   img_index = i + BATCH_SIZE * batch_num
+   if (img_index >= num_images): break
+   image = images[img_index]
+   outputFile.write(image +',' + ','.join(['%.16f' % num for num in probs[i]]) + '\n')
+
+outputFile.close()
