d4ab715c2e95776a97399c4ef550c1391b3f427e
Alex Zamoshchin <alexzamoshchin@gmail.com>
2015-03-12 00:25:47 -0400

    augment data version 2.0

--- scripts/augment_data.py
+++ scripts/augment_data.py
@@ -1,15 +1,18 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import shutil
 import os
+import random
 
 class_names_file = open("../data/alphabetical_class_list.txt")
 class_folders = [line.strip() for line in class_names_file]
 
 OLD_TRIAN_DIR = '../data/train'
-NEW_TRAIN_DIR = '../data/train_aug'
+NEW_TRAIN_DIR = '../data/train_aug_2'
 if os.path.exists(NEW_TRAIN_DIR):
   shutil.rmtree(NEW_TRAIN_DIR)
 os.makedirs(NEW_TRAIN_DIR)
+
+NUM_TO_ADD = 15
 
 for i,class_folder in enumerate(class_folders):
   print "Class", i, "/ 121"
@@ -18,9 +21,14 @@ for i,class_folder in enumerate(class_folders):
   for image in images:
     if image == '.DS_Store': continue
     im = Image.open(OLD_TRIAN_DIR + '/' + class_folder + '/' + image)
-    for rotation_degrees in [0, 90, 180, 270]:
-      new_im = im.rotate(rotation_degrees)
-      new_im.save(NEW_TRAIN_DIR + '/' + class_folder + '/' + image.split('.')[0] + "_" + str(rotation_degrees) + '.jpg')
+    im = ImageOps.invert(im)
+
+    for img_num in range(NUM_TO_ADD):
+      rotation_degree = img_num*(360/NUM_TO_ADD) + random.randint(0, (360/NUM_TO_ADD))
+      new_im = im.rotate(rotation_degree, expand=1)
+      new_im = ImageOps.invert(new_im)
+      
+      new_im.save(NEW_TRAIN_DIR + '/' + class_folder + '/' + image.split('.')[0] + "_" + str(rotation_degree) + '.jpg')
 		  
 		
 		
