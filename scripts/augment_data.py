a4e4de03871f1a1f29fa8384567945552cd3df4d
Alex Zamoshchin <alexzamoshchin@gmail.com>
2015-03-13 19:28:51 -0400

    aug data version 3.0

--- scripts/augment_data.py
+++ scripts/augment_data.py
@@ -7,12 +7,12 @@ class_names_file = open("../data/alphabetical_class_list.txt")
 class_folders = [line.strip() for line in class_names_file]
 
 OLD_TRIAN_DIR = '../data/train'
-NEW_TRAIN_DIR = '../data/train_aug_2'
+NEW_TRAIN_DIR = '../data/train_aug_3'
 if os.path.exists(NEW_TRAIN_DIR):
   shutil.rmtree(NEW_TRAIN_DIR)
 os.makedirs(NEW_TRAIN_DIR)
 
-NUM_TO_ADD = 15
+NUM_TO_ADD = 10
 
 for i,class_folder in enumerate(class_folders):
   print "Class", i, "/ 121"
@@ -24,11 +24,10 @@ for i,class_folder in enumerate(class_folders):
     im = ImageOps.invert(im)
 
     for img_num in range(NUM_TO_ADD):
-      rotation_degree = img_num*(360/NUM_TO_ADD) + random.randint(0, (360/NUM_TO_ADD))
+      rotation_degree = img_num*(180/NUM_TO_ADD) + random.randint(0, (180/NUM_TO_ADD))
       new_im = im.rotate(rotation_degree, expand=1)
       new_im = ImageOps.invert(new_im)
       
       new_im.save(NEW_TRAIN_DIR + '/' + class_folder + '/' + image.split('.')[0] + "_" + str(rotation_degree) + '.jpg')
 		  
-		
 		
